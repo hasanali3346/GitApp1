e0c41c599c2c77e838e152bdff65f1320c2401a4
Hasan Ali DONMEZ <hasanali12091@gmail.com>
2024-05-24 22:23:52 +0300

    v1

---
+++ src/main/java/Runner.java
@@ -0,0 +1,9 @@
+public class Runner {
+    public static void main(String[] args) {
+
+        //versiyon 1
+        System.out.println("hello GIT");
+        System.out.println("git add . : staging areaya alir.");
+
+    }
+}
